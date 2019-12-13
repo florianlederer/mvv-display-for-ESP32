4e8d7989274ad43acc6579435d5a7bf3551dba35
jbob <mail@markusko.ch>
2019-12-13 20:40:35 +0100

    Big refactor. Should take care of #11

--- MVV_ESP/MVV_ESP.ino
+++ MVV_ESP/MVV_ESP.ino
@@ -2,144 +2,207 @@
 #include <HTTPClient.h>
 #define ARDUINOJSON_USE_LONG_LONG 1
 #include <ArduinoJson.h>
- 
+
 #include <Arduino.h>
 #include "Wire.h"
 #include "SSD1306.h"
- 
+
 #include "time.h"
- 
-const char* ssid = "XXX";
-const char* password =  "XXX";
- 
+
+enum types {
+    mvg_api,
+    geops_api
+};
+
+const int MAX_INCLUDE_LINE = 10;
+const int MAX_EXCLUDE_DESTINATION = 10;
+
+typedef struct {
+    const char *wifi_name;
+    const char *wifi_pass;
+    const enum types type;
+    const char *url;
+    const char *include_line[MAX_INCLUDE_LINE];
+    const char *exclude_destinations[MAX_EXCLUDE_DESTINATION];
+} Config;
+#include "config.h";
+Config config;
+
 //https://www.mvg.de/dienste/abfahrtszeiten.html
-const char* url =  "https://www.mvg.de/api/fahrinfo/departure/de:09162:6?footway=0";
- 
+//const char* url =  "https://www.mvg.de/api/fahrinfo/departure/de:09162:6?footway=0";
+
 SSD1306 display (0x3c, 4, 15);
- 
+
 StaticJsonDocument<20000> doc;
- 
+
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 3600;
 const int   daylightOffset_sec = 3600;
- 
-void setup() {
- 
-  Serial.begin(115200);
- 
-  pinMode (16, OUTPUT);
-  digitalWrite (16, LOW); // set GPIO16 low to reset OLED
-  delay (50);
-  digitalWrite (16, HIGH); // while OLED is running, GPIO16 must go high
- 
-  delay(4000);   //Delay needed before calling the WiFi.begin
- 
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
- 
-  Serial.println("Connected to the WiFi network");
- 
-  display.init ();
-  display.flipScreenVertically ();
-  display.setFont (ArialMT_Plain_10);
-  delay(1000);
- 
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
- 
-}
- 
-void loop() {
- 
- 
-  if (WiFi.status() == WL_CONNECTED) {
- 
-    HTTPClient http;
- 
-    http.begin(url);
-    int httpResponseCode = http.GET();
- 
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println("NEW DATA");
-      Serial.println(httpResponseCode);
-      Serial.println(response);
- 
-      Serial.println("Parsing Json...");
-      //Parsing Json
-      DeserializationError error = deserializeJson(doc, response);
-      if (error)
-      {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.c_str());
-      }
-      else
-      {
-        Serial.println("No Errors");
-        String servings = doc["departures"][0];
-        Serial.println(servings);
- 
-        time_t now;
-        time(&now);
-        Serial.println(now);
- 
-        display.clear();
-        unsigned int departures_length = doc["departures"].size();
-        unsigned int i = 0;
-        unsigned int cnt = 0;
- 
-        while (i < departures_length && cnt < 4)
-        {
-          if (doc["departures"][i]["product"] == "SBAHN")
-          {
-            //Calc minutes until departure
-            unsigned long departure = doc["departures"][i]["departureTime"].as<long long>() / 1000; //ms to seconds
-            Serial.println(departure);
- 
-            unsigned long minutes = 0;
-            if ( departure > now)
-            {
-              unsigned long wait = departure - now;
-              Serial.println(wait);
-              minutes = wait / 60;
-              if (wait % 60 > 30) ++minutes;
-              minutes +=  doc["departures"][i]["delay"].as<int>();
+const int   number_of_configs = sizeof(configs) / sizeof(*configs);
+int config_number = -1;
+
+int connect_wifi() {
+    int number_of_networks = WiFi.scanNetworks();
+    if (number_of_networks == -1 ) {
+        Serial.println("No networks available");
+    }
+    for (int i=0; i<number_of_networks; ++i) {
+        String ssid = WiFi.SSID(i);
+        // is this network in config...?
+        for (int j=0; j<number_of_configs; ++j) {
+            if (strcmp(ssid.c_str(), configs[j].wifi_name) == 0) {
+                // ... yes it is
+                WiFi.begin(configs[j].wifi_name, configs[j].wifi_pass);
+                while (WiFi.status() != WL_CONNECTED) {
+                    delay(1000);
+                    Serial.println("Connecting to WiFi...");
+                }
+                Serial.printf("Connected to the WiFi network: %s\n", ssid.c_str());
+                return j;
             }
-            Serial.println(minutes);
- 
-            String label = doc["departures"][i]["label"].as<String>();
-            String destination  = doc["departures"][i]["destination"].as<String>();
-            String full_name = label + " " + destination;
- 
-            display.setTextAlignment (TEXT_ALIGN_LEFT);
-            display.setFont (ArialMT_Plain_10);
-            display.drawString( 0 , cnt * 16, full_name);
- 
-            display.setFont (ArialMT_Plain_16);
-            display.setTextAlignment (TEXT_ALIGN_RIGHT);
-            display.drawString( 128, cnt * 16, String(minutes));
-            ++cnt;
-          }
-          ++i;
         }
-        display.display();
-      }
- 
+    }
+    return -1;
+}
+
+void setup_display() {
+    display.init ();
+    display.flipScreenVertically ();
+    display.setFont (ArialMT_Plain_10);
+    delay(1000);
+}
+
+void handle_mvg_api(Config config) {
+    HTTPClient http;
+
+    http.begin(config.url);
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode > 0) {
+        String response = http.getString();
+        Serial.println("New data");
+        Serial.println(httpResponseCode);
+        Serial.println(response);
+
+        Serial.println("Parsing JSON...");
+        DeserializationError error = deserializeJson(doc, response);
+        if (error) {
+            Serial.print(F("deserializeJson() failed: "));
+            Serial.println(error.c_str());
+        } else {
+            Serial.println("No errors");
+            String servings = doc["departures"][0];
+            Serial.println(servings);
+
+            time_t now;
+            time(&now);
+            Serial.println(now);
+
+            display.clear();
+            unsigned int departures_length = doc["departures"].size();
+            unsigned int i = 0;
+            unsigned int cnt = 0;
+
+            while (i < departures_length && cnt < 4) {
+                // Extract what we are interested in from response
+                // based on config
+                bool interesting_line = false;
+                bool interesting_destination = true;
+                for (int j=0; j<MAX_INCLUDE_LINE; ++j) {
+                    Serial.println(config.include_line[j]);
+                    if (config.include_line[j] && strcmp(config.include_line[j], "*") == 0) {
+                        // We want to see all lines
+                        interesting_line = true;
+                        break;
+                    }
+                    if (config.include_line[j] && strcmp(config.include_line[j], doc["departures"][i]["label"]) == 0) {
+                        // We want to see this line
+                        interesting_line = true;
+                        break;
+                    }
+                }
+                if (interesting_line) {
+                    for (int j=0; j<MAX_EXCLUDE_DESTINATION; ++j) {
+                        if (config.exclude_destinations[j] && strcmp(config.exclude_destinations[j], doc["departures"][i]["destination"]) == 0) {
+                            interesting_destination = false;
+                            break;
+                        }
+                    }
+                }
+                if (interesting_line && interesting_destination) {
+                    //Calc minutes until departure
+                    unsigned long departure = doc["departures"][i]["departureTime"].as<long long>() / 1000; //ms to seconds
+                    Serial.println(departure);
+
+                    unsigned long minutes = 0;
+                    if ( departure > now) {
+                        unsigned long wait = departure - now;
+                        Serial.println(wait);
+                        minutes = wait / 60;
+                        if (wait % 60 > 30) ++minutes;
+                        minutes +=  doc["departures"][i]["delay"].as<int>();
+                    }
+                    Serial.println(minutes);
+
+                    String label = doc["departures"][i]["label"].as<String>();
+                    String destination  = doc["departures"][i]["destination"].as<String>();
+                    String full_name = label + " " + destination;
+
+                    display.setTextAlignment (TEXT_ALIGN_LEFT);
+                    display.setFont (ArialMT_Plain_10);
+                    display.drawString( 0 , cnt * 16, full_name);
+
+                    display.setFont (ArialMT_Plain_16);
+                    display.setTextAlignment (TEXT_ALIGN_RIGHT);
+                    display.drawString( 128, cnt * 16, String(minutes));
+                    ++cnt;
+                }
+                ++i;
+            }
+            display.display();
+        }
     } else {
-      Serial.print("Error: Couldn't send GET: ");
-      Serial.println(httpResponseCode);
+        Serial.print("Error: Couldn't send GET: ");
+        Serial.println(httpResponseCode);
     }
     http.end();
- 
-  } else {
- 
-    Serial.println("Error in WiFi connection");
- 
-  }
- 
-  delay(30000);  //Send a request every 30 seconds
- 
+}
+
+void setup() {
+
+    Serial.begin(115200);
+
+    pinMode (16, OUTPUT);
+    digitalWrite (16, LOW); // set GPIO16 low to reset OLED
+    delay (50);
+    digitalWrite (16, HIGH); // while OLED is running, GPIO16 must go high
+
+    setup_display();
+
+    delay(4000);   //Delay needed before calling the WiFi.begin
+
+    while (config_number == -1) {
+        config_number = connect_wifi();
+    }
+
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    config = configs[config_number];
+
+}
+
+void loop() {
+    if (WiFi.status() == WL_CONNECTED) {
+        switch (config.type) {
+            case mvg_api: {
+                handle_mvg_api(config);
+                break;
+            }
+            default: {
+                Serial.println("Unkown config type");
+            }
+        }
+    } else {
+        Serial.println("Error in WiFi connection");
+    }
+    delay(30000);  //Send a request every 30 seconds
 }
--- README.md
+++ README.md
@@ -1,32 +1,32 @@
 # mvv display for ESP32
 Zeigt die nächsten Abfahrten der MVV auf einem kleinem OLED Display an. Es handelt sich hierbei um kein Projekt der MVV.
- 
+
 ## Benötigtes:
 - ESP32 mit OLED Display: https://www.amazon.de/dp/B076P8GRWV/
 - Micro USB Kabel
 - USB Netzteil
 - Computer zum Programmieren
- 
+
 ## Anleitung:
 1.  Arduino IDE installieren: https://www.arduino.cc/en/main/software
-2.  ESP32 Board installieren: https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
-3.  TOOLS -> Manage Libraries.. anklicken und nach der "ESP8266 and ESP32 OLED driver for SSD1306 displays" (ThingPulse) suchen und installieren. Anschließend nach der "Arduinojson" suchen und ebenfalls installieren.
-4.  Tools -> Board -> "TTGO LoEa-32-OLED V1" auswählen.
-5.  Tools ->  Port anschauen und die Enträge merken falls nicht ausgeblendet.
-6.  ESP32 mit dem Kabel mit dem Computer verbinden und erneut unter Tools ->  Port schauen nun müsste ein neuer Port hinzugekommen sein/ Port ist nicht mehr ausgeblendet. Diesen nun auswählen.
-7.  MVV_ESP/MVV_ESP.ino aus diesem Repo kopieren und in der Arduino IDE einfügen.
-8.  Sketch -> Verify/Compile und schauen ob irgendwelche Fehler in der Konsole auftreten. Gegebenfalls die Libraries überprüfen.
-9. In den Zeilen const char* ssid = "XXX"; sowie const char* password =  "XXX"; die drei XXX jeweils durch WLAN Namen und Passwort ersetzten.
-10. Die Seite https://www.mvg.de/dienste/abfahrtszeiten.html im Firefox Webbrowser öffnen und anschließend STRG + SHIFT + E drücken. Jetzt sollte ein neues Fenster aufgegangen sein welches den Netzwerkverkehr mitschneidet. 
-11. Nun den gewünschten Bahnhof ganz normal eingeben und sich die aktuellen Abfahrten anzeigen lassen.
-12. Im in Schritt 10 geöffneten Fenster bei Type nach dem/den Paketen mit json suchen und die Bahnhof nun in der Spalte File rauslesen.
-13. Das was in File steht muss man nun in der Zeile const char* url =.... am Ende ersetzen.
-14. Sketch -> Upload
-15. Jetzt sollten auf dem Display die aktuellen Abfahrten der Sbahn angezeigt werden.
-16. Zum Debuggen kann der Serielle Monitor unter Tools -> Serial Monitor aufgerufen werden.
+1.  ESP32 Board installieren: https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
+1.  Tools -> Manage Libraries.. anklicken und nach der "ESP8266 and ESP32 OLED driver for SSD1306 displays" (ThingPulse) suchen und installieren. Anschließend nach der "Arduinojson" suchen und ebenfalls installieren.
+1.  Tools -> Board -> "TTGO LoRa-32-OLED V1" auswählen.
+1.  Tools ->  Port anschauen und die Enträge merken falls nicht ausgeblendet.
+1.  ESP32 mit dem Kabel mit dem Computer verbinden und erneut unter Tools ->  Port schauen nun müsste ein neuer Port hinzugekommen sein/ Port ist nicht mehr ausgeblendet. Diesen nun auswählen.
+1. MVV_ESP/MVV_ESP.ino aus diesem Repo kopieren und in der Arduino IDE einfügen.
+1. Sketch -> Verify/Compile und schauen ob irgendwelche Fehler in der Konsole auftreten. Gegebenfalls die Libraries überprüfen.
+1. config.h.dist kopieren nach config.dist und mindestens eine eigene Konfiguration eintragen
+  1. Die Seite https://www.mvg.de/dienste/abfahrtszeiten.html im Firefox Webbrowser öffnen und anschließend STRG + SHIFT + E drücken. Jetzt sollte ein neues Fenster aufgegangen sein welches den Netzwerkverkehr mitschneidet.
+  1. Nun den gewünschten Bahnhof ganz normal eingeben und sich die aktuellen Abfahrten anzeigen lassen.
+  1. Im in Schritt 9.i geöffneten Fenster bei Type nach dem/den Paketen mit json suchen und die Bahnhof nun in der Spalte File rauslesen.
+  1. Die ermittelte URL in die config.h eintragen
+1. Sketch -> Upload
+1. Jetzt sollten auf dem Display die aktuellen Abfahrten angezeigt werden.
+1. Zum Debuggen kann der Serielle Monitor unter Tools -> Serial Monitor aufgerufen werden.
 
 ## API:
 Aktuell wird die MVG API verwendet. Diese liefert Infos für alle Verkehrsmittel der MVG (Bus/Sbahn/Ubahn/Tram). Sie stellt die Grundlage für die MVG Website sowie die MVG Naviagtor App dar. Allerdings sind die Echtzeitinformationen besonders bei Störungen nicht optimal. Aus diesem Grund gibt es einen neuen Branch der sich mit der geops-API, die unteranderem in der München Navigator APP oder auf s-bahn-muenchen-live.de zum Einsatz kommt, beschäftigt. Hier bekommt man sehr genaue Echtzeitinformationen, allerdings ausschließlich für die Sbahn.
 
 ## Jodel:
-[https://shared.jodel.com/58NiubGCf2?channel=other](https://shared.jodel.com/58NiubGCf2?channel=other) 
+[https://shared.jodel.com/58NiubGCf2?channel=other](https://shared.jodel.com/58NiubGCf2?channel=other)
