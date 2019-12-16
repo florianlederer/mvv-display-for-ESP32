faef0b7a13822f291100c18ae65653cbbfa0cd7a
florianlederer <florian.ledt@t-online.de>
2019-12-16 21:11:37 +0100

    cleanup

--- MVV_ESP/MVV_ESP.ino
+++ MVV_ESP/MVV_ESP.ino
@@ -54,45 +54,58 @@ void drawTopLine()
     tft.drawString("Min", 220, 0);
 }
 
-void drawSbahn(int y_display, char * line, char * destination, int track, int wagon, int minutes)
+void setupDisplay()
+{   tft.init();
+    tft.setRotation(3);
+    //tft.fillScreen(TFT_BLACK);
+    tft.fillScreen(0x005);
+    if (TFT_BL > 0) { // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+         pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
+         digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
+    }
+}
+
+void drawDepature(int display_line, String line, String destination, int track, int wagon, int minutes)
 {
   char str_buffer[10];
-
+  int line_height = 15;
+  int offset = 9;
+  int y_display = display_line*line_height+offset;
     
-    tft.setFreeFont(FF17);
-    tft.setTextDatum(TL_DATUM);
+  tft.setFreeFont(FF17);
+  tft.setTextDatum(TL_DATUM);
 
-    //line
-    tft.fillRoundRect(0, y_display, 28, 15, 7,TFT_MAGENTA);
-    tft.setTextColor(0x005);
-    tft.drawString(line, 3, y_display); 
+  //line
+  tft.fillRoundRect(0, y_display, 28, 15, 7,TFT_MAGENTA);
+  tft.setTextColor(0x005);
+  tft.drawString(line, 3, y_display); 
 
-    //destination
-    tft.setTextColor(TFT_WHITE);
-    tft.drawString(destination, 32, y_display);
+  //destination
+  tft.setTextColor(TFT_WHITE);
+  tft.drawString(destination, 32, y_display);
 
-    //track
-    sprintf(str_buffer, "%u", track);
-    tft.drawString(str_buffer, 150, y_display);
+  //track
+  sprintf(str_buffer, "%u", track);
+  tft.drawString(str_buffer, 150, y_display);
 
-    //wagon
-    tft.drawString("_ _ _", 167, y_display);
-    switch(wagon)
-    {
-      case 3:
-        tft.fillRoundRect(195, y_display+4, 14, 11, 3,TFT_WHITE); //rechts
-      case 2:
-        tft.fillRoundRect(165, y_display+4, 14, 11, 3,TFT_WHITE); //links
-      case 1:
-        tft.fillRoundRect(180, y_display+4, 14, 11, 3,TFT_WHITE); //mitte
-       default:
-          break;
-    }
+  //wagon
+  tft.drawString("_ _ _", 167, y_display);
+  switch(wagon)
+  {
+    case 3:
+      tft.fillRoundRect(195, y_display+4, 14, 11, 3,TFT_WHITE); //rechts
+    case 2:
+      tft.fillRoundRect(165, y_display+4, 14, 11, 3,TFT_WHITE); //links
+    case 1:
+      tft.fillRoundRect(180, y_display+4, 14, 11, 3,TFT_WHITE); //mitte
+      default:
+        break;
+  }
 
-    //minutes
-    tft.setTextDatum(TR_DATUM);
-    sprintf(str_buffer, "%u", minutes);
-    tft.drawString(str_buffer, 240,y_display);
+  //minutes
+  tft.setTextDatum(TR_DATUM);
+  sprintf(str_buffer, "%u", minutes);
+  tft.drawString(str_buffer, 240,y_display);
   
 }
 
@@ -100,89 +113,23 @@ void setup()
 {
     Serial.begin(115200);
     Serial.println("Start");
-    tft.init();
-    tft.setRotation(3);
-    //tft.fillScreen(TFT_BLACK);
-    tft.fillScreen(0x005);
-
-    
-    
-
-    if (TFT_BL > 0) { // TFT_BL has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-         pinMode(TFT_BL, OUTPUT); // Set backlight pin to output mode
-         digitalWrite(TFT_BL, TFT_BACKLIGHT_ON); // Turn backlight on. TFT_BACKLIGHT_ON has been set in the TFT_eSPI library in the User Setup file TTGO_T_Display.h
-    }
-
-    drawTopLine();
-    
-    int line_height = 15;
-    int offset = 9;
-    
-    drawSbahn(0*line_height+offset, "S3", "Maisach", 1, 3, 1);
-    drawSbahn(1*line_height+offset, "S3", "Holzkirchen", 1, 2, 10);
-    drawSbahn(2*line_height+offset, "S3", "Deisenhofen", 1, 1, 100);
-    drawSbahn(3*line_height+offset, "S3", "Maisach", 1, 3, 1);
-    drawSbahn(4*line_height+offset, "S3", "Holzkirchen", 1, 2, 10);
-    drawSbahn(5*line_height+offset, "S3", "Deisenhofen", 1, 1, 100);
-    drawSbahn(6*line_height+offset, "S3", "Maisach", 1, 3, 1);
-    drawSbahn(7*line_height+offset, "S3", "Holzkirchen", 1, 2, 10);
-    /*
-    tft.setFreeFont(FF17);
-    tft.setTextDatum(TL_DATUM);
-
-    tft.fillRoundRect(0, 0, 28, 15, 7,TFT_MAGENTA);
-    tft.setTextColor(0x005);
-    tft.drawString("S3", 3, 0); 
-    
-    tft.setTextColor(TFT_WHITE);
-    tft.drawString("Holzkirchen", 32, 0);
-     
-    tft.drawString("1", 150, 0);
-
-    tft.drawString("_ _ _", 167, 0);
-    tft.fillRoundRect(165, 3, 14, 11, 3,TFT_WHITE); //links
-    tft.fillRoundRect(180, 3, 14, 11, 3,TFT_WHITE); //mitte
-    tft.fillRoundRect(195, 3, 14, 11, 3,TFT_WHITE); //rechts
-
-    tft.drawString("13", 220,0);
-    */
-
-    /*
-    tft.setTextFont(GLCD);
-    tft.drawString("19:17 Sauerlach -> Deisenhofen: 60%", 0, 20);
-    tft.drawLine(0,30,240,30, TFT_WHITE);
-    /*
-    
-    
-    //tft.drawString("Der Halt am Ostbahnhof entfällt!", 0, 30);
-    //tft.drawLine(0,40,240,40, TFT_WHITE);
-   
-    /*
-    tft.drawStr;ing("S3 Holzkirchen 2", 0, 16);
-    tft.drawString("S3 Holzkirchen 3", 0, 32);
-    tft.drawString("S3 Holzkirchen 4", 0, 48);
-    tft.drawString("S3 Holzkirchen 5", 0, 64);
-    tft.drawString("S3 Holzkirchen 6", 0, 80);
-    tft.drawString("S3 Holzkirchen 7", 0, 96);
-    tft.drawString("S3 Holzkirchen 8", 0, 112);
-    */
-    
-    //tft.fillCircle(160,120,5,TFT_RED);
-
-
-    
-    //tft.fillTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color);
-    //tft.fillRect(0, 0, 28, 15, 7,TFT_MAGENTA)
-
-    
-
-
+    setupDisplay();
 
 
 }
 
 
 void loop()
-{
-
+{   tft.fillScreen(0x005);
+    delay(2000);
+    drawTopLine();
+    drawDepature(0, "S3", "Maisach", 1, 3, 1);
+    drawDepature(1, "S3", "Holzkirchen", 1, 2, 10);
+    drawDepature(2, "S3", "Deisenhofen", 1, 1, 100);
+    drawDepature(3, "S3", "Maisach", 1, 3, 1);
+    drawDepature(4, "S3", "Holzkirchen", 1, 2, 10);
+    drawDepature(5, "S3", "Deisenhofen", 1, 1, 100);
+    drawDepature(6, "S3", "Maisach", 1, 3, 1);
+    drawDepature(7, "S3", "Holzkirchen", 1, 2, 10);
+    delay(2000);
 }
